75f66ed6f09d986bf18db6061b514b2b733a16a4
SamuelF2 <samueltfries@gmail.com>
2024-03-20 12:05:25 -0500

    Created item class with getters and setters.

---
+++ PrimeTimeApp/src/main/java/org/primeTimeHotel/Domain_Model_Objects/Item.java
@@ -0,0 +1,54 @@
+package org.primeTimeHotel.Domain_Model_Objects;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Item {
+    private int id;
+    private double price;
+    private String name;
+    private String description;
+    private Image image;
+
+    private static int idCounter = 0;
+
+    public Item(){
+        this.id = idCounter;
+        idCounter++;
+    }
+    public Item(String itemName, String itemDescription, int itemId, double itemPrice, Image img){
+        this.name = itemName;
+        this.description = itemDescription;
+        this.id = itemId;
+        this.price = itemPrice;
+        this.image = img;
+    }
+
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public void setId(int id) {this.id = id;}
+    public void setPrice(double price) {this.price = price;}
+    public void setImage(Image image) {this.image = image;}
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public int getID(){return this.id;}
+    public double getPrice() {return price;}
+    public Image getImage() {return image;}
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Item item = (Item) object;
+        return id == item.id && Double.compare(price, item.price) == 0 && Objects.equals(name, item.name) && Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, price, name, description);
+    }
+
+
+
+
+}
