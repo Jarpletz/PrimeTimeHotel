43ba3b738270f74fc3f210701667279eb0e215e8
Andrew Arthur <33909620+akarthur04@users.noreply.github.com>
2024-03-23 12:50:15 -0500

    Did not need the top statement

--- PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/ExampleDatabaseConnection.java
+++ PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/ExampleDatabaseConnection.java
@@ -6,9 +6,6 @@ public class ExampleDatabaseConnection {
     public static void main(String[] args) {
         Connection connection = null;
         try {
-            // Load the Oracle JDBC driver
-            Class.forName("oracle.jdbc.driver.OracleDriver"); // idk why it does this lol
-
             // Connect to the database
             String url = "jdbc:oracle:thin:@prime-time-hotel.c70qkmwawtvx.us-east-2.rds.amazonaws.com:1521:orcl";
             String username = "prime_time_admin";
@@ -20,11 +17,11 @@ public class ExampleDatabaseConnection {
 
             // Execute a statement to create a table
             String createTableSql = "CREATE TABLE test_table (id NUMBER, name VARCHAR2(50))";
-            statement.execute(createTableSql);
+            //statement.execute(createTableSql);
 
             // Execute a statement to insert data into the table
             String insertDataSql = "INSERT INTO test_table (id, name) VALUES (1, 'Test')";
-            statement.execute(insertDataSql);
+            //statement.execute(insertDataSql);
 
             // Execute a query to select data from the table
             String selectDataSql = "SELECT * FROM test_table";
@@ -36,7 +33,7 @@ public class ExampleDatabaseConnection {
                 String name = resultSet.getString("name");
                 System.out.println("id: " + id + ", name: " + name);
             }
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             if (connection != null) {
