59831caae0870d75b88e76d8f308715b30e59e85
Jarpletz <rothpletz5@gmail.com>
2024-03-24 15:36:22 -0500

    Basic stuff for account DAO

--- PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/AccountDAO.java
+++ PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/AccountDAO.java
@@ -13,7 +13,7 @@ public class AccountDAO extends  MasterDAO{
 
     public Account fetchAccount(int account_id){
         try {
-            PreparedStatement statement =connection.prepareStatement("SELECT * FROM ACCOUNTS WHERE ID = ?");
+            PreparedStatement statement =connection.prepareStatement("SELECT * FROM ACCOUNTS WHERE ID = ? FETCH FIRST ROW ONLY");
             statement.setInt(1,account_id);
             List<Account> accounts= fetchAccounts(statement);
             if(!accounts.isEmpty()) return accounts.getFirst();
@@ -25,14 +25,69 @@ public class AccountDAO extends  MasterDAO{
     }
 
     public boolean insert(Account account){
-
-        return true;
+        if(connection== null) return false;
+        if(fetchAccount(account.getId())==null){
+            try{
+                PreparedStatement stmt = connection.prepareStatement(
+                        "INSERT INTO ACCOUNTS(USERNAME,PASSWORD,FIRST_NAME,LAST_NAME,PHONE_NUMBER,EMAIL,ACCOUNT_TYPE)" +  " VALUES (?,?,?,?,?,?,?)");
+                stmt.setString(1,account.getUsername());
+                stmt.setString(2, account.getPassword());
+                stmt.setString(3,account.getFirstName());
+                stmt.setString(4,account.getLastName());
+                stmt.setString(5,account.getPhoneNumber());
+                stmt.setString(6,account.getEmail());
+                stmt.setInt(7,account.getType().getCode());
+                stmt.executeUpdate();
+                return true;
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
     public boolean update(Account account){
-        return true;
+        if(connection== null) return false;
+        if(fetchAccount(account.getId())!=null) return false;
+        try{
+            PreparedStatement stmt = connection.prepareStatement(
+                    "UPDATE  ACCOUNTS "+
+                            "SET USERNAME=?,PASSWORD=?,FIRST_NAME=?,LAST_NAME=?,PHONE_NUMBER=?,EMAIL=?,ACCOUNT_TYPE=?" +
+                            "WHERE id = ?");
+            stmt.setString(1,account.getUsername());
+            stmt.setString(2, account.getPassword());
+            stmt.setString(3,account.getFirstName());
+            stmt.setString(4,account.getLastName());
+            stmt.setString(5,account.getPhoneNumber());
+            stmt.setString(6,account.getEmail());
+            stmt.setInt(7,account.getType().getCode());
+            stmt.setInt(8,account.getId());
+            stmt.executeUpdate();
+            return true;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
+    public boolean delete(Account account){
+        if(connection== null) return false;
+        if(fetchAccount(account.getId())!=null){
+            try{
+                PreparedStatement stmt = connection.prepareStatement(
+                        "DELETE FROM ACCOUNTS WHERE ID = ?");
+                stmt.setInt(1,account.getId());
+                stmt.executeUpdate();
+                return true;
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
     //fetches ALL accounts
     public List<Account> fetchAllAccounts(){
         List<Account> accounts = new ArrayList<>();
@@ -61,7 +116,21 @@ public class AccountDAO extends  MasterDAO{
         }
     }
 
+    public List<Account> fetchByUserInfo(String searchQuery){
 
+        try {
+            PreparedStatement statement =connection.prepareStatement("SELECT * FROM ACCOUNTS " +
+                    "WHERE UPPER(EMAIL) LIKE ? OR UPPER(FIRST_NAME) LIKE ? OR UPPER(LAST_NAME) LIKE ? OR UPPER(PHONE_NUMBER) LIKE ?"+
+                    "FETCH FIRST 10 ROWS ONLY");
+            for(int i=1; i<= 4;i++){
+                statement.setString(i,searchQuery.toUpperCase());
+            }
+            return fetchAccounts(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
     private List<Account> fetchAccounts(PreparedStatement statement){
