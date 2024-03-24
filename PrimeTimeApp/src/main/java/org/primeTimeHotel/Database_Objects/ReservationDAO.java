5020653e9e523f29a8190c6147c03023c2987cde
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-03-24 15:16:38 -0500

    Revert "Merge remote-tracking branch 'origin' into AddRoom_Case"
    This reverts commit 0ed9a08a0d90c5b2084d2e8f1ba0080ff7b1dcd7, reversing
    changes made to f304f6f1d5b561d060f9bc2591fc30b45260c7da.

--- PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/ReservationDAO.java
+++ PrimeTimeApp/src/main/java/org/primeTimeHotel/Database_Objects/ReservationDAO.java
@@ -14,11 +14,17 @@ import java.util.Scanner;
 public class ReservationDAO extends  MasterDAO {
     private List<Reservation> tempData;
 
+
+    public ReservationDAO(){
+        super();
+    }
+
     public Reservation fetchReservation(int reservationId){
         if(connection == null) return null;
-        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM reservations WHERE id = ?")){
+        try {
+            PreparedStatement statement =connection.prepareStatement("SELECT * FROM reservations WHERE id = ?");
             statement.setInt(1,reservationId);
-            ResultSet rs = statement.executeQuery();
+            ResultSet  rs= statement.executeQuery();
             List<Reservation> reservations = resultSetToReservationList(rs);
             if(reservations.isEmpty()) return null;
             return  reservations.getFirst();
@@ -142,7 +148,14 @@ public class ReservationDAO extends  MasterDAO {
     private List<Reservation> resultSetToReservationList(ResultSet rs) throws SQLException {
         List<Reservation> reservations= new ArrayList<>();
         while (rs.next()) {
-            reservations.add(new Reservation(rs));
+            Reservation reservation = new Reservation();
+            reservation.setId(rs.getInt("id"));
+            reservation.setUserId(rs.getInt("user_id"));
+            reservation.setRoomId(rs.getInt("room_id"));
+            reservation.setStartDate(rs.getDate("start_date"));
+            reservation.setEndDate(rs.getDate("end_date"));
+            reservation.setStatus(ReservationStatus.values()[rs.getInt("status")]);
+            reservations.add(reservation);
         }
         return reservations;
     }
